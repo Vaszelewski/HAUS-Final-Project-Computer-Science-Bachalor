8380ee9fa61d6622acfc8ee4b3d680fd47eb034c
Everton Vaszelewski <everton.vaszelewski@hotmail.com>
2020-05-03 18:16:10 -0300

    criacao de cookie e exibicao de notificacoes apos reload

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -14,4 +14,32 @@ function exibeNotificacao(acao, msg){
 	{
 		alertify.error("Erro! " + msg);
 	}
-}	
+}
+
+function criaCookie(nome, valor, tempo) {
+	let data = new Date(new Date().getTime() + tempo * 60 * 60 * 1000);
+	document.cookie = nome + "=" + valor + ";expires=" + data.toUTCString();
+}
+
+function deletaCookie(nome){
+	criaCookie(nome, "", -1);
+}
+
+function buscaCookie(nome){
+	nome = nome + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let arrayCookie = decodedCookie.split(';');
+
+	for(let i = 0; i <arrayCookie.length; i++){
+		let cookiePartes = arrayCookie[i];
+
+		while (cookiePartes.charAt(0) == ' '){
+			cookiePartes = cookiePartes.substring(1);
+		}
+
+		if (cookiePartes.indexOf(nome) == 0){
+			return cookiePartes.substring(nome.length, cookiePartes.length);
+		}
+	}
+	return "";
+}
